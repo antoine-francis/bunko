62c4e87831ca9e75a013f0c1eef96b6ef98cb698
Antoine Francis <44614977+antoine-francis@users.noreply.github.com>
2025-03-03 16:31:52 -0500

    Add footer skeleton

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,26 +1,7 @@
-import {Link} from "react-router-dom";
-import {paths} from "@/config/paths.ts";
-import {useBunkoDispatch, useBunkoSelector} from "@/hooks/redux-hooks.ts";
-import {useCallback} from "react";
-import {toggleVerticalBar} from "@/slices/UiStateSlice.ts";
-import {IconMenu2, IconPlus} from "@tabler/icons-react";
-import {Search} from "../search/Search.tsx";
-import {URL} from "@/constants/Url.ts";
-
-
 export const Footer = () => {
-	const {user} = useBunkoSelector((state) => state.currentUser);
-	const dispatch = useBunkoDispatch();
-
-	const toggleOtherBar = useCallback(() => {
-		dispatch(toggleVerticalBar());
-	}, [dispatch]);
-
-	if (user) {
-		return (
-			<footer>
-				Privacy - Terms - Cookies © {new Date().getFullYear()} Antoine Francis
-			</footer>
-		)
-	}
+	return (
+		<footer>
+			Privacy - Terms - Cookies © {new Date().getFullYear()} Antoine Francis
+		</footer>
+	)
 }
