710acce08593a8787aaf9ddebb63b794a2d27a89
Antoine Francis <44614977+antoine-francis@users.noreply.github.com>
2025-03-07 11:18:37 -0500

    Fix picture URL routing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = 'api/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
