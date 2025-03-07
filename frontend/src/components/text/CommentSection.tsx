397154bf5bb3075d63227fd41c4782a0bd6d71ea
Antoine Francis <44614977+antoine-francis@users.noreply.github.com>
2025-03-07 10:57:49 -0500

    Removed isFixed prop

--- frontend/src/components/text/CommentSection.tsx
+++ frontend/src/components/text/CommentSection.tsx
@@ -16,9 +16,6 @@ export function CommentSection({comments, text} : CommentSectionProps ) {
 		<div id="comments-container">
 			<hr/>
 			{comments.map((comment: BunkoComment) => {
-				const isLiked : boolean = comment.author !== undefined ? comment.likes !== undefined && comment.likes.filter((like) => {
-					return like.user.username === comment.author.username
-				}).length > 0 : false;
 				return (
 					<React.Fragment key={comment.id}>
 							<Comment comment={comment} text={text}/>
